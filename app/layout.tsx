7a0dbeb7d5cba9a8798198838d92d7700e34a5ff
tribotools25-lab <tribotools25@gmail.com>
2025-12-26 10:31:36 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className="dashcode-app">{children}</body>
     </html>
   );
 }
