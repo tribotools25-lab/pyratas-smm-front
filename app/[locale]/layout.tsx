4caa6bf0a8814ee6a22e9ca7b861373a40c658dd
tribotools25-lab <tribotools25@gmail.com>
2025-12-26 00:48:39 -0300

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,17 +1,23 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
+// Providers
 import { ThemeProvider } from "@/providers/theme-provider";
 import MountedProvider from "@/providers/mounted.provider";
+import DirectionProvider from "@/providers/direction-provider";
+import AuthProvider from "@/providers/auth.provider";
+
+// UI
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "@/components/ui/sonner";
-const inter = Inter({ subsets: ["latin"] });
-// language
+
+// i18n
 import { getLangDir } from "rtl-detect";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import DirectionProvider from "@/providers/direction-provider";
-import AuthProvider from "@/providers/auth.provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pyratas — Social Media Marketing",
@@ -21,15 +27,16 @@ export const metadata: Metadata = {
 export default async function RootLayout({
   children,
   params: { locale },
-}: Readonly<{
+}: {
   children: React.ReactNode;
   params: { locale: string };
-}>) {
+}) {
   const messages = await getMessages();
   const direction = getLangDir(locale);
+
   return (
     <html lang={locale} dir={direction}>
-      <body className={`${inter.className} dashcode-app `}>
+      <body className={`${inter.className} dashcode-app`}>
         <NextIntlClientProvider messages={messages} locale={locale}>
           <AuthProvider>
             <ThemeProvider
@@ -43,6 +50,8 @@ export default async function RootLayout({
                   {children}
                 </DirectionProvider>
               </MountedProvider>
+
+              {/* Notifications */}
               <Toaster />
               <SonnerToaster />
             </ThemeProvider>
@@ -52,4 +61,3 @@ export default async function RootLayout({
     </html>
   );
 }
-
