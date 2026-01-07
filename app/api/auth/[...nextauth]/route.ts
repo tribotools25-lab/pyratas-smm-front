5f5602a44a0339c20a7cb9ec1e4c8727ba848588
tribotools25-lab <tribotools25@gmail.com>
2026-01-07 20:58:55 -0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth";
 
-export const { handlers } = NextAuth(authConfig);
+const auth = NextAuth(authConfig);
 
-export const GET = handlers.GET;
-export const POST = handlers.POST;
+export const GET = auth.handlers.GET;
+export const POST = auth.handlers.POST;
