ec48305aaf6d5375f3ef8086288a02014977efb1
tribotools25-lab <tribotools25@gmail.com>
2026-01-09 14:10:14 -0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,6 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth";
-
-const { handlers } = NextAuth(authConfig);
+import { handlers } from "@/auth";
 
 export const GET = handlers.GET;
 export const POST = handlers.POST;
 
-// (opcional, mas ajuda o Next a não tentar Edge)
 export const runtime = "nodejs";
