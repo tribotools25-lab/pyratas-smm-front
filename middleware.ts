09ae5c1fc6dc5e54767640dfe8e5d38d5f9d580b
tribotools25-lab <tribotools25@gmail.com>
2026-01-10 09:24:29 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,11 @@
 import createMiddleware from "next-intl/middleware";
-import { NextRequest, NextResponse } from "next/server";
-import { locales, defaultLocale } from "./config";
+import {locales, defaultLocale} from "./config";
 
-const intlMiddleware = createMiddleware({
+export default createMiddleware({
   locales,
-  defaultLocale,
+  defaultLocale
 });
 
-export default function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-
-  // 🔴 NUNCA intercepta API
-  if (pathname.startsWith("/api")) {
-    return NextResponse.next();
-  }
-
-  // aplica i18n
-  const response = intlMiddleware(req);
-
-  // páginas públicas
-  if (pathname.includes("/login")) return response;
-
-  return response;
-}
-
 export const config = {
-  matcher: ["/((?!_next|favicon.ico|.*\\..*).*)"],
+  matcher: ["/((?!api|_next|.*\\..*).*)"]
 };
