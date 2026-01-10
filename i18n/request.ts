9c46fcc8ecbdf41a3125721733b98f183451c2b1
tribotools25-lab <tribotools25@gmail.com>
2026-01-10 09:23:33 -0300

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,15 +1,9 @@
-import {getRequestConfig} from 'next-intl/server';
-import {routing} from './routing';
- 
-export default getRequestConfig(async ({requestLocale}) => {
-  let locale = await requestLocale;
+import {getRequestConfig} from "next-intl/server";
 
-  if (!locale || !routing.locales.includes(locale as any)) {
-       locale = routing.defaultLocale;
-     }
- 
+export default getRequestConfig(async ({locale}) => {
+  const messages = (await import(`../messages/${locale}.json`)).default;
+
   return {
-    locale,
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages
   };
 });
