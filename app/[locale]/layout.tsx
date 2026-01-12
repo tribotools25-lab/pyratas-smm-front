4ceb86942f257986c096d36bf71413ef64efcf80
tribotools25-lab <tribotools25@gmail.com>
2026-01-12 13:36:48 -0300

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,61 +1,25 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import {NextIntlClientProvider} from "next-intl";
+import {getMessages, unstable_setRequestLocale} from "next-intl/server";
 
-// Providers
-import { ThemeProvider } from "@/providers/theme-provider";
-import MountedProvider from "@/providers/mounted.provider";
-import DirectionProvider from "@/providers/direction-provider";
-import AuthProvider from "@/providers/auth.provider";
+export function generateStaticParams() {
+  return [{locale: "pt"}, {locale: "en"}];
+}
 
-// UI
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as SonnerToaster } from "@/components/ui/sonner";
-
-// i18n
-import { getLangDir } from "rtl-detect";
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Pyratas — Social Media Marketing",
-  description: "created by codeshaper",
-};
-
-export default async function RootLayout({
+export default async function LocaleLayout({
   children,
-  params: { locale },
+  params
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: {locale: string};
 }) {
+  unstable_setRequestLocale(params.locale);
   const messages = await getMessages();
-  const direction = getLangDir(locale);
 
   return (
-    <html lang={locale} dir={direction}>
-      <body className={`${inter.className} dashcode-app`}>
-        <NextIntlClientProvider messages={messages} locale={locale}>
-          <AuthProvider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <MountedProvider>
-                <DirectionProvider direction={direction}>
-                  {children}
-                </DirectionProvider>
-              </MountedProvider>
-
-              {/* Notifications */}
-              <Toaster />
-              <SonnerToaster />
-            </ThemeProvider>
-          </AuthProvider>
+    <html lang={params.locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
         </NextIntlClientProvider>
       </body>
     </html>
