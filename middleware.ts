40c9dcc69484a233641dde470b3c04242058c6de
tribotools25-lab <tribotools25@gmail.com>
2026-01-12 17:21:30 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
+// middleware.ts
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-  locales: ['en', 'pt'],
+  locales: ['pt', 'en'],
   defaultLocale: 'pt'
 });
 
 export const config = {
-  matcher: ['/', '/(pt|en)/:path*']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
