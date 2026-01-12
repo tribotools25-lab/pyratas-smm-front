9d3e19ad312072e799abe31e288a575aa287c63b
tribotools25-lab <tribotools25@gmail.com>
2026-01-12 13:34:47 -0300

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,9 +1,9 @@
 import {getRequestConfig} from "next-intl/server";
 
 export default getRequestConfig(async ({locale}) => {
+  // Se seu projeto usa /messages, beleza.
+  // Se usa /src/messages, ajuste o path.
   const messages = (await import(`../messages/${locale}.json`)).default;
 
-  return {
-    messages
-  };
+  return {messages};
 });
